1ac9a72b521ee98f586b9507d434d005f85b8952
Jerry-kjk <jerrykajik@gmail.com>
2023-08-30 22:53:22 +0300

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * depth - compares size.
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: the height of the tree. If tree is NULL, return 0.
  */
